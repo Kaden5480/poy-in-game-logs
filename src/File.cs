73574f673b7e5c4bc86c98a5b49b3afc3c43961a
Kaden5480 <kaden5480@protonmail.com>
2025-12-30 15:39:42 +0000

    Implement file logging

---
+++ src/File.cs
@@ -0,0 +1,138 @@
+using System.IO;
+
+using BepInEx;
+using UnityEngine;
+
+namespace InGameLogs {
+    /**
+     * <summary>
+     * Holds the file logging source.
+     * </summary>
+     */
+    internal class File {
+        private static File instance;
+
+        // Log location
+        private string directory;
+        private string filePath;
+
+        // Writer
+        private StreamWriter writer;
+
+        /**
+         * <summary>
+         * Initializes the file logging source.
+         * </summary>
+         */
+        internal File() {
+            instance = this;
+
+            directory = Path.Combine(
+                Paths.GameRootPath, "InGameLogs"
+            );
+
+            MakeWriter();
+        }
+
+        /**
+         * <summary>
+         * Creates a file writer.
+         * </summary>
+         */
+        private void MakeWriter() {
+            // Make the directory if it doesn't exist
+            if (Directory.Exists(directory) == false) {
+                Directory.CreateDirectory(directory);
+            }
+
+            // Determine whether to use separate files
+            if (Config.File.separateFiles.Value == true) {
+                filePath = Path.Combine(
+                    directory, $"{Logs.timeNowSafe}.log"
+                );
+            }
+            else {
+                filePath = Path.Combine(
+                    directory, $"InGameLogs.log"
+                );
+            }
+
+            // And create the writer
+            writer = new StreamWriter(
+                filePath, Config.File.appendLogs.Value
+            ) {
+                AutoFlush = true,
+            };
+        }
+
+        /**
+         * <summary>
+         * Adds a log to the file.
+         * </summary>
+         * <param name="level">The log level</param>
+         * <param name="data">The log data to add</param>
+         * <param name="bepin">Whether the log originated from BepInEx</param>
+         */
+        private void AddLog(string level, object data, bool bepin) {
+            // Don't log if disabled
+            if (Config.File.enabled.Value == false) {
+                return;
+            }
+
+            // Ignore BepInEx logs
+            if (Config.File.bepinLogs.Value == false && bepin == true) {
+                return;
+            }
+
+            writer.WriteLine($"[{Logs.timeNow}] {data}");
+        }
+
+        /**
+         * <summary>
+         * Adds a debug log.
+         * </summary>
+         * <param name="data">The data to add</param>
+         * <param name="bepin">Whether the log originated from BepInEx</param>
+         */
+        internal static void AddDebug(object data, bool bepin) {
+            // Don't log if debug history is disabled
+            if (Config.File.debugHistory.Value == false || instance == null) {
+                return;
+            }
+
+            instance.AddLog("Debug", data, bepin);
+        }
+
+        /**
+         * <summary>
+         * Adds an info log.
+         * </summary>
+         * <param name="data">The data to add</param>
+         * <param name="bepin">Whether the log originated from BepInEx</param>
+         */
+        internal static void AddInfo(object data, bool bepin) {
+            // Don't log if info history is disabled
+            if (Config.File.infoHistory.Value == false || instance == null) {
+                return;
+            }
+
+            instance.AddLog("Info", data, bepin);
+        }
+
+        /**
+         * <summary>
+         * Adds an error log.
+         * </summary>
+         * <param name="data">The data to add</param>
+         * <param name="bepin">Whether the log originated from BepInEx</param>
+         */
+        internal static void AddError(object data, bool bepin) {
+            // Don't log if error history is disabled
+            if (Config.File.errorHistory.Value == false || instance == null) {
+                return;
+            }
+
+            instance.AddLog("Error", data, bepin);
+        }
+    }
+}
---
+++ src/config/File.cs
@@ -0,0 +1,79 @@
+using BepInEx.Configuration;
+using ModMenu.Config;
+
+namespace InGameLogs.Config {
+    /**
+     * <summary>
+     * Holds the file logging config.
+     * </summary>
+     */
+    internal static class File {
+        [Field("Enabled")]
+        internal static ConfigEntry<bool> enabled { get; private set; }
+
+        [Field("Separate Files")]
+        internal static ConfigEntry<bool> separateFiles { get; private set; }
+
+        [Field("Append Logs")]
+        internal static ConfigEntry<bool> appendLogs { get; private set; }
+
+        [Field("BepInEx Logs")]
+        internal static ConfigEntry<bool> bepinLogs { get; private set; }
+
+        [Field("Debug History")]
+        internal static ConfigEntry<bool> debugHistory { get; private set; }
+
+        [Field("Info History")]
+        internal static ConfigEntry<bool> infoHistory { get; private set; }
+
+        [Field("Error History")]
+        internal static ConfigEntry<bool> errorHistory { get; private set; }
+
+        /**
+         * <summary>
+         * Initializes the file logging config.
+         * </summary>
+         * <param name="configFile">The config file to bind to</param>
+         */
+        internal static void Init(ConfigFile configFile) {
+            enabled = configFile.Bind(
+                "File", "enabled", true,
+                "Whether logging to a file should be enabled."
+            );
+
+            separateFiles = configFile.Bind(
+                "File", "separateFiles", false,
+                "Whether a separate file should be made for each game session."
+            );
+
+            appendLogs = configFile.Bind(
+                "File", "appendLogs", false,
+                "If `Separate Files` is disabled, whether logs should be appended to the log file."
+                + " If this is disabled, the log file will be overwritten for each game session."
+            );
+
+            bepinLogs = configFile.Bind(
+                "File", "bepinLogs", false,
+                "Whether BepInEx logs should also be written to log files."
+            );
+
+            // Debug
+            debugHistory = configFile.Bind(
+                "File", "debugHistory", true,
+                "Whether debug logs should be logged to a file."
+            );
+
+            // Info
+            infoHistory = configFile.Bind(
+                "File", "infoHistory", true,
+                "Whether info logs should be logged to a file."
+            );
+
+            // Error
+            errorHistory = configFile.Bind(
+                "File", "errorHistory", true,
+                "Whether error logs should be logged to a file."
+            );
+        }
+    }
+}
